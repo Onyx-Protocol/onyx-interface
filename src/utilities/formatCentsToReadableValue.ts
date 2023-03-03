6564c78abaafa429bd8b001ecb659c2c53463100
zraccoon <andrei1120@outlook.com>
2023-03-03 11:33:09 -0800

    update price decimal

--- src/utilities/formatCentsToReadableValue.ts
+++ src/utilities/formatCentsToReadableValue.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import BigNumber from 'bignumber.js';
 import { shortenValueWithSuffix } from 'utilities';
 
@@ -17,7 +18,15 @@ const formatCentsToReadableValue = ({
   const wrappedValueDollars = new BigNumber(value).dividedBy(100);
 
   if (!shortenLargeValue) {
-    return `$${wrappedValueDollars.toFormat(2)}`;
+    return `$${
+      wrappedValueDollars.gt(0.01)
+        ? wrappedValueDollars.toFormat(2)
+        : wrappedValueDollars.gt(0.001)
+        ? wrappedValueDollars.toFormat(3)
+        : wrappedValueDollars.gt(0.0001)
+        ? wrappedValueDollars.toFormat(4)
+        : wrappedValueDollars.toFormat(5)
+    }`;
   }
 
   // Shorten value
