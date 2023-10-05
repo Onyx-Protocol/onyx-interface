bfa8d188a74ae956934723bc7748d091bb4c1a3a
Alex <sampekmoy@gmail.com>
2023-10-05 23:07:07 +0400

    move to footer

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -11,6 +11,7 @@ import { useGetBlockNumber } from 'clients/api';
 import { Icon } from 'components/Icon';
 import tokenAddresses from 'constants/contracts/addresses/tokens.json';
 
+import LanguageSwitch from '../Header/LanguageSwitch';
 import {
   XCN_DISCORD_URL,
   XCN_GITHUB_URL,
@@ -30,51 +31,57 @@ export const FooterUi: React.FC<FooterUiProps> = ({ currentBlockNumber }) => {
 
   return (
     <div css={styles.container}>
-      {!!currentBlockNumber && (
-        <Typography
-          component="a"
-          variant="small2"
-          css={styles.blockInfo}
-          href={config.ethScanUrl}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {t('footer.latestNumber')}
-          <br css={styles.blockInfoMobileLineBreak} />
-          <span css={styles.blockInfoNumber}>{currentBlockNumber}</span>
-        </Typography>
-      )}
+      <div css={styles.languageSwitch}>
+        <LanguageSwitch />
+      </div>
 
-      <div css={styles.links}>
-        <a
-          css={styles.link}
-          href={generateEthScanUrl(tokenAddresses.xcn[config.chainId])}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Icon name="xcn" color={styles.theme.palette.text.primary} size="12px" />
-        </a>
+      <div css={styles.rightContainer}>
+        {!!currentBlockNumber && (
+          <Typography
+            component="a"
+            variant="small2"
+            css={styles.blockInfo}
+            href={config.ethScanUrl}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {t('footer.latestNumber')}
+            <br css={styles.blockInfoMobileLineBreak} />
+            <span css={styles.blockInfoNumber}>{currentBlockNumber}</span>
+          </Typography>
+        )}
 
-        <a css={styles.link} href={XCN_MEDIUM_URL} target="_blank" rel="noreferrer">
-          <Icon name="medium" color={styles.theme.palette.text.primary} size="12px" />
-        </a>
+        <div css={styles.links}>
+          <a
+            css={styles.link}
+            href={generateEthScanUrl(tokenAddresses.xcn[config.chainId])}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Icon name="xcn" color={styles.theme.palette.text.primary} size="12px" />
+          </a>
 
-        <a css={styles.link} href={XCN_DISCORD_URL} target="_blank" rel="noreferrer">
-          <img src={discordImg} alt="discord" width="20px" height="20px" />
-          {/* <Icon name="discord" color={styles.theme.palette.text.primary} size="12px" /> */}
-        </a>
+          <a css={styles.link} href={XCN_MEDIUM_URL} target="_blank" rel="noreferrer">
+            <Icon name="medium" color={styles.theme.palette.text.primary} size="12px" />
+          </a>
 
-        <a css={styles.link} href={XCN_TELEGRAM_URL} target="_blank" rel="noreferrer">
-          <Icon name="telegram" color={styles.theme.palette.text.primary} size="12px" />
-        </a>
+          <a css={styles.link} href={XCN_DISCORD_URL} target="_blank" rel="noreferrer">
+            <img src={discordImg} alt="discord" width="20px" height="20px" />
+            {/* <Icon name="discord" color={styles.theme.palette.text.primary} size="12px" /> */}
+          </a>
 
-        <a css={styles.link} href={XCN_TWITTER_URL} target="_blank" rel="noreferrer">
-          <Icon name="twitter" color={styles.theme.palette.text.primary} size="12px" />
-        </a>
+          <a css={styles.link} href={XCN_TELEGRAM_URL} target="_blank" rel="noreferrer">
+            <Icon name="telegram" color={styles.theme.palette.text.primary} size="12px" />
+          </a>
 
-        <a css={styles.link} href={XCN_GITHUB_URL} target="_blank" rel="noreferrer">
-          <Icon name="github" color={styles.theme.palette.text.primary} size="12px" />
-        </a>
+          <a css={styles.link} href={XCN_TWITTER_URL} target="_blank" rel="noreferrer">
+            <Icon name="twitter" color={styles.theme.palette.text.primary} size="12px" />
+          </a>
+
+          <a css={styles.link} href={XCN_GITHUB_URL} target="_blank" rel="noreferrer">
+            <Icon name="github" color={styles.theme.palette.text.primary} size="12px" />
+          </a>
+        </div>
       </div>
     </div>
   );
