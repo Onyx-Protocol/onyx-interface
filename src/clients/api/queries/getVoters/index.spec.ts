c75656140a1e7caf9330f69f7fca8063b8453f33
r2moon <ryuheimat3@gmail.com>
2023-10-20 02:03:41 +0900

    fixed some faild cases

--- src/clients/api/queries/getVoters/index.spec.ts
+++ src/clients/api/queries/getVoters/index.spec.ts
@@ -1,20 +1,52 @@
-import { restService } from 'utilities';
+import config from 'config';
+import { enableFetchMocks } from 'jest-fetch-mock';
 
 import votersResponse from '__mocks__/api/voters.json';
+import { SUBGRAPH_LINKS } from 'constants/endpoints';
 
 import getVoters from '.';
 
-jest.mock('utilities/restService');
+enableFetchMocks();
 
 describe('api/queries/getVoters', () => {
   test('returns proposal', async () => {
-    (restService as jest.Mock).mockImplementationOnce(async () => ({
-      status: 200,
-      data: votersResponse,
-    }));
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        status: 200,
+        data: {
+          proposalVotes: votersResponse,
+        },
+      }),
+    );
 
     const response = await getVoters({
       id: '1',
+    });
+
+    expect(fetchMock).toBeCalledWith(SUBGRAPH_LINKS[config.chainId].latest, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          query proposalVotesQuery {
+            proposalVotes(
+              ${''},
+              where: { proposal: "1" ${''} }
+            ) {
+              id
+              proposal
+              address
+              support
+              votes
+              blockNumber
+              blockTimestamp
+              transactionHash
+            }
+          }
+        `,
+      }),
     });
 
     expect(response).toMatchSnapshot();
