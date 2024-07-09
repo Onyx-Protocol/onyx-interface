d4821b44825915c2daa93b6bf44266fb1b9cfe35
ozdav97 <ozdav97@gmail.com>
2024-07-09 06:58:51 +0100

    updated x link

--- src/components/Layout/Footer/constants.ts
+++ src/components/Layout/Footer/constants.ts
@@ -1,5 +1,5 @@
 export const XCN_MEDIUM_URL = 'https://medium.com/OnyxProtocol';
 export const XCN_DISCORD_URL = 'https://discord.com/invite/onyxprotocol';
 export const XCN_TELEGRAM_URL = 'https://t.me/OnyxOrg';
-export const XCN_TWITTER_URL = 'https://twitter.com/OnyxProtocol';
+export const XCN_TWITTER_URL = 'https://x.com/OnyxDAO';
 export const XCN_GITHUB_URL = 'https://GitHub.com/Onyx-Protocol';
