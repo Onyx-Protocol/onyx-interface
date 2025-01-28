29b25d443e4d6c406a01066e5b680596e39d285f
Onyxcoin <admin@onyx.org>
2025-01-28 18:18:40 +0100

    Update twitter-X tabs
    Typo fix on iconUI

--- src/components/Icon/icons/twitterX.tsx
+++ src/components/Icon/icons/twitterX.tsx
@@ -12,8 +12,7 @@ const SvgTwitterX = (props: SVGProps<SVGSVGElement>) => (
   >
     <g clipPath="url(#clip0_319_1906)">
       <path
-        d="M10,0C4.5,0,0,4.5,0,10s4.5,10,10,10s10-4.5,10-10S15.5,0,10,0z M12.5,6h1.3l-2.9,3.4l3.5,4.6h-2.7l-2.1-2.8
-		L7.1,14H5.8l3.2-3.6L5.6,6h2.8l1.9,2.5L12.5,6L12.5,6z M12.1,13.2h0.7L8,6.8H7.2L12.1,13.2L12.1,13.2z"
+        d="M10,0C4.5,0,0,4.5,0,10s4.5,10,10,10s10-4.5,10-10S15.5,0,10,0z M12.5,6h1.3l-2.9,3.4l3.5,4.6h-2.7l-2.1-2.8 L7.1,14H5.8l3.2-3.6L5.6,6h2.8l1.9,2.5L12.5,6L12.5,6z M12.1,13.2h0.7L8,6.8H7.2L12.1,13.2L12.1,13.2z"
         fill="white"
       />
     </g>
@@ -26,7 +25,3 @@ const SvgTwitterX = (props: SVGProps<SVGSVGElement>) => (
 );
 
 export default SvgTwitterX;
-
-
-
-
