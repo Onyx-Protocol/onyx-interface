a685b02cfa028a948b6ac14ffc22d52385188cb9
Onyx_dev <24818322+matri8not@users.noreply.github.com>
2025-06-15 00:41:44 +0200

    pagination fix

--- src/components/Layout/constants.tsx
+++ src/components/Layout/constants.tsx
@@ -99,14 +99,15 @@ export const menuItems: MenuItem[] = [
   //   icon: 'wpunksOutlined',
   // },
   // TO BE ACTIVATED ONCE CALCULATOR IS READY
-  // {
-  //     href: Path.GOVERNANCE_LEADER_BOARD,
-  //     // Translation key: do not remove this comment
-  //     // t('layout.menuItems.xcn')
-  //     i18nKey: 'layout.menuItems.leaderboard',
-  //     i18nTitleKey: 'layout.menuItems.leaderboard',
-  //     icon: 'xcnOutlined',
-  // },
+  {
+    href: Path.GOVERNANCE_LEADER_BOARD,
+    // Translation key: do not remove this comment
+    // t('layout.menuItems.xcn')
+    i18nKey: 'layout.menuItems.leaderboard',
+    i18nTitleKey: 'layout.menuItems.leaderboard',
+    isNew: true,
+    icon: 'xcnOutlined',
+  },
   {
     href: Path.POINTS,
     // Translation key: do not remove this comment
