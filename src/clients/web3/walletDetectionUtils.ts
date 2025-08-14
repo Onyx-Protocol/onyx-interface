22b979973cd3d4cb34746bebd4fa41ba8b8a2382
colex2 <coletoo122@gmail.com>
2025-08-14 18:15:38 +0200

    fix: remove unused util for wallet detection, fix performance issue

--- src/clients/web3/walletDetectionUtils.ts
+++ src/clients/web3/walletDetectionUtils.ts
@@ -1,36 +1,3 @@
 export const isRunningInOperaBrowser = () => window.ethereum?.isOpera;
 
 export const isRunningInBinanceChainWallet = () => !!window.BinanceChain;
-
-export const detectProvider = async (timeout = 3000): Promise<any> => {
-  let handled = false;
-
-  return new Promise(resolve => {
-    function handleProvider() {
-      if (handled) return;
-      handled = true;
-      window.removeEventListener('ethereum#initialized', handleProvider);
-      resolve(window.ethereum);
-    }
-
-    if (window.ethereum) {
-      handleProvider();
-    } else {
-      window.addEventListener('ethereum#initialized', handleProvider, { once: true });
-
-      const interval = setInterval(() => {
-        if (window.ethereum) {
-          handleProvider();
-        }
-      }, 100);
-
-      setTimeout(() => {
-        if (!handled) {
-          clearInterval(interval);
-          window.removeEventListener('ethereum#initialized', handleProvider);
-          resolve(null);
-        }
-      }, timeout);
-    }
-  });
-};
